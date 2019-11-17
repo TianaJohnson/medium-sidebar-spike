50d4eb62979664c4aa1340b9469381a933842802
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-17 12:21:01 -0600

    className sidebar

--- src/App.js
+++ src/App.js
@@ -15,23 +15,7 @@ const items = [
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
       <Sidebar items={items} />
-    </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,19 +4,17 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 
-function Sidebar() {
+function Sidebar({items}) {
   return (
+      <div className="sidebar">
     <List disablePadding dense>
-      <ListItem button>
-        <ListItemText>Home</ListItemText>
+    {items.map(({ label, name, ...rest }) => (
+      <ListItem key={name} button {...rest}>
+        <ListItemText>{label}</ListItemText>
       </ListItem>
-      <ListItem button>
-        <ListItemText>Billing</ListItemText>
-      </ListItem>
-      <ListItem button>
-        <ListItemText>Settings</ListItemText>
-      </ListItem>
-    </List>
+    ))}
+  </List>
+  </div>
   )
 }
 
