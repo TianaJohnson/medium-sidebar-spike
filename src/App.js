1c6cfe9a9c6d4c07817628571b72e14c94cb6d1c
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-17 12:24:08 -0600

    out with the old

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Sidebar';
 
+//out with the old
+// const items = [
+//   { name: 'home', label: 'Home' },
+//   { name: 'billing', label: 'Billing' },
+//   { name: 'settings', label: 'Settings' },
+// ]
+
+//in with the new
 const items = [
   { name: 'home', label: 'Home' },
-  { name: 'billing', label: 'Billing' },
-  { name: 'settings', label: 'Settings' },
+  {
+    name: 'billing',
+    label: 'Billing',
+    items: [
+      { name: 'statements', label: 'Statements' },
+      { name: 'reports', label: 'Reports' },
+    ],
+  },
+  {
+    name: 'settings',
+    label: 'Settings',
+    items: [{ name: 'profile', label: 'Profile' }],
+  },
 ]
-
-
 
 
 function App() {
