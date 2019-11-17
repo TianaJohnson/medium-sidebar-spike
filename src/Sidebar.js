5496660f08431ffb737d2e582d533e85ebbc579d
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-17 13:06:12 -0600

    sidebar iems componenet inception

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,41 +4,83 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 
-function Sidebar({items}) {
-  return (
-<div className="sidebar">
-      <List disablePadding dense>
-        {items.map(({ label, name, items: subItems, ...rest }) => {
-          return (
-            <React.Fragment key={name}>
-              <ListItem style={{ paddingLeft: 18 }} button {...rest}>
-                <ListItemText>{label}</ListItemText>
-              </ListItem>
-              {Array.isArray(subItems) ? (
-                <List disablePadding dense>
-                  {subItems.map((subItem) => {
-                    return (
-                      <ListItem
-                        key={subItem.name}
-                        style={{ paddingLeft: 36 }}
-                        button
-                        dense
-                      >
-                        <ListItemText>
-                          <span className="sidebar-subitem-text">
-                            {subItem.label}
-                          </span>
-                        </ListItemText>
-                      </ListItem>
-                    )
-                  })}
-                </List>
-              ) : null}
-            </React.Fragment>
-          )
-        })}
-      </List>
-    </div>
+function SidebarItem({ label, items, depthStep = 10, depth = 0, ...rest }) {
+    return (
+      <>
+        <ListItem button dense {...rest}>
+          <ListItemText style={{ paddingLeft: depth * depthStep }}>
+            <span>{label}</span>
+          </ListItemText>
+        </ListItem>
+        {Array.isArray(items) ? (
+          <List disablePadding dense>
+            {items.map((subItem) => (
+              <SidebarItem
+                key={subItem.name}
+                depth={depth + 1}
+                depthStep={depthStep}
+                {...subItem}
+              />
+            ))}
+          </List>
+        ) : null}
+      </>
+    )
+  }
+  
+  function Sidebar({ items, depthStep, depth }) {
+    return (
+      <div className="sidebar">
+        <List disablePadding dense>
+          {items.map((sidebarItem, index) => (
+            <SidebarItem
+              key={`${sidebarItem.name}${index}`}
+              depthStep={depthStep}
+              depth={depth}
+              {...sidebarItem}
+            />
+          ))}
+        </List>
+      </div>
+    )
+  }
+
+    
+
+// 4th
+// <div className="sidebar">
+//       <List disablePadding dense>
+//         {items.map(({ label, name, items: subItems, ...rest }) => {
+//           return (
+//             <React.Fragment key={name}>
+//               <ListItem style={{ paddingLeft: 18 }} button {...rest}>
+//                 <ListItemText>{label}</ListItemText>
+//               </ListItem>
+//               {Array.isArray(subItems) ? (
+//                 <List disablePadding dense>
+//                   {subItems.map((subItem) => {
+//                     return (
+//                       <ListItem
+//                         key={subItem.name}
+//                         style={{ paddingLeft: 36 }}
+//                         button
+//                         dense
+//                       >
+//                         <ListItemText>
+//                           <span className="sidebar-subitem-text">
+//                             {subItem.label}
+//                           </span>
+//                         </ListItemText>
+//                       </ListItem>
+//                     )
+//                   })}
+//                 </List>
+//               ) : null}
+//             </React.Fragment>
+//           )
+//         })}
+//       </List>
+//     </div> 
 
     // 3rd
     // <div className="sidebar">
@@ -85,7 +127,5 @@ function Sidebar({items}) {
 //       ))}
 //     </List>
 //   </div>
-  )
-}
 
 export default Sidebar
