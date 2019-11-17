1b298ab758ed4c2243b7971d8ff88b454e5b44ff
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-17 14:48:23 -0600

    oo I like that

--- src/App.js
+++ src/App.js
@@ -1,37 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Sidebar from './Sidebar';
+import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
+import SettingsIcon from "@material-ui/icons/Settings";
+import Sidebar from "./Sidebar";
 
-//out with the old
-// const items = [
-//   { name: 'home', label: 'Home' },
-//   { name: 'billing', label: 'Billing' },
-//   { name: 'settings', label: 'Settings' },
-// ]
+function onClick(e, item) {
+  window.alert(JSON.stringify(item, null, 2));
+}
 
-//in with the new
 const items = [
-  { name: 'home', label: 'Home' },
+  { name: "home", label: "Home", Icon: HomeIcon },
   {
-    name: 'billing',
-    label: 'Billing',
+    name: "billing",
+    label: "Billing",
+    Icon: ReceiptIcon,
     items: [
-      { name: 'statements', label: 'Statements' },
-      { name: 'reports', label: 'Reports' },
-    ],
+      { name: "statements", label: "Statements", onClick },
+      { name: "reports", label: "Reports", onClick }
+    ]
   },
+  "divider",
   {
-    name: 'settings',
-    label: 'Settings',
-    items: [{ name: 'profile', label: 'Profile' }],
-  },
-]
-
+    name: "settings",
+    label: "Settings",
+    Icon: SettingsIcon,
+    items: [
+      { name: "profile", label: "Profile" },
+      { name: "insurance", label: "Insurance", onClick },
+      "divider",
+      {
+        name: "notifications",
+        label: "Notifications",
+        Icon: NotificationsIcon,
+        items: [
+          { name: "email", label: "Email", onClick },
+          {
+            name: "desktop",
+            label: "Desktop",
+            Icon: DesktopWindowsIcon,
+            items: [
+              { name: "schedule", label: "Schedule" },
+              { name: "frequency", label: "Frequency" }
+            ]
+          },
+          { name: "sms", label: "SMS" }
+        ]
+      }
+    ]
+  }
+];
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Sidebar items={items} />
     </div>
   );
